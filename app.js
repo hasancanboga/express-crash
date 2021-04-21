b612e04d85465d0a1bd0c0d5a13f30a3eaa08000
hasancanboga <hasancanboga@gmail.com>
2021-04-21 16:15:57 +0300

    installed mongoose and connected to mongo atlas

--- app.js
+++ app.js
@@ -1,113 +1,30 @@
-const Joi = require('joi')
 const express = require('express')
+const mongoose = require('mongoose')
+require('dotenv/config')
 
-const app = express()
-app.use(express.json())
+const app = express();
 
-const courses = [
-    { id: 1, name: "course1" },
-    { id: 2, name: "course2" },
-    { id: 3, name: "course3" },
-]
-
+// ROUTES
 
 app.get('/', (req, res) => {
-    res.send('Hello World')
+    res.send('We are on home')
 })
 
-app.get('/courses', (req, res) => {
-    res.send(courses)
+app.get('/posts', (req, res) => {
+    console.log('Das ist actual route');
+    res.send('We are on posts')
 })
 
-app.get('/courses/:id', (req, res) => {
-    course = courses.find(c => c.id == req.params.id)
-    if (!course) {
-        return res.status(404).send('The course with the given ID was not found')
+// Connecto to DB
+mongoose.connect(process.env.DB_CONNECTION, { useNewUrlParser: true, useUnifiedTopology: true }, (err) => {
+    if (err) {
+        console.log(err)
+    } else {
+        console.log('Connected to DB!')
     }
-    res.send(course)
 })
 
-app.get('/courses/:year/:month', (req, res) => {
-    res.send(req.params) // url params
-    res.send(req.query) // query string params (?asd=1)
-})
+// start listening the server
 
-app.post('/courses', (req, res) => {
-    // send a post request with { "name": "asa" } to this url in postman. note that url is same as get request, but using post makes it another route.
+app.listen(3000);
 
-    const { error } = validateCourse(req.body) // this is called object destructuring. the error const is set to the .error property of whatver validateCourse returns.
-
-    if (error) {
-        // 400 Bad Request
-        return res.status(400).send(error.details[0].message)
-    }
-
-    const course = {
-        id: courses.length + 1,
-        name: req.body.name
-    }
-
-    courses.push(course)
-    res.send(course)
-})
-
-app.put('/courses/:id', (req, res) => {
-    // Look up the course
-    // If not existing, return 404
-    course = courses.find(c => c.id == req.params.id)
-    if (!course) {
-        return res.status(404).send('The course with the given ID was not found')
-    }
-
-    // Validate
-    // If invalid, return 400 - Bad request
-    const { error } = validateCourse(req.body)
-
-    if (error) {
-        // 400 Bad Request
-        return res.status(400).send(error.details[0].message)
-    }
-
-    // Update course
-    course.name = req.body.name
-    // Return the updated course
-    res.send(course)
-})
-
-app.delete('/courses/:id', (req, res) => {
-    // Look up the course
-    // If not existing, return 404
-    course = courses.find(c => c.id == req.params.id)
-    if (!course) {
-        return res.status(404).send('The course with the given ID was not found')
-    }
-
-    // Delete
-
-    const index = courses.indexOf(course)
-    courses.splice(index,1)
-
-    res.send(course)
-})
-
-
-
-
-
-
-
-function validateCourse(course) {
-    const schema = Joi.object({
-        name: Joi.string().min(3).required()
-    })
-
-    return schema.validate(course)
-}
-
-
-
-
-
-
-const port = process.env.PORT ?? 3000
-app.listen(port, () => console.log(`Listening on port ${port}`))
